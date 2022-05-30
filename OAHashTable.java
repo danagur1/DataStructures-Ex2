23f5d86450d29f07d477647150aab0e9fd6ce967
danagur1 <danagur1@mail.tau.ac.il>
2022-05-30 12:34:23 +0300

    implemented OAHashTable

--- OAHashTable.java
+++ OAHashTable.java
@@ -2,27 +2,56 @@
 public abstract class OAHashTable implements IHashTable {
 	
 	private HashTableElement [] table;
+	//hash table element to mark deleted element cell:
+	private static final HashTableElement deleted = new HashTableElement(-1, -1); 
 	
 	public OAHashTable(int m) {
 		this.table = new HashTableElement[m];
-		// TODO add to constructor as needed
 	}
 	
 	
 	@Override
 	public HashTableElement Find(long key) {
-		// TODO implement find
-		return null;
+		for (int i=0; i<table.length; i++) { //search series:
+			int j = Hash(key, i); //the hash result index
+			if (table[j] == null) { //empty cell
+				return null; //stop searching
+			}
+			else if (table[j].GetKey() == key) { 
+				return table[j];
+			}
+		}
+		return null; //series ended
 	}
 	
 	@Override
 	public void Insert(HashTableElement hte) throws TableIsFullException,KeyAlreadyExistsException {
-		// TODO implement insertion	
+		long key = hte.GetKey();
+		for (int i=0; i<table.length; i++) { //search series:
+			int j = Hash(key, i); //the hash result index
+			if ((table[j] == null) || (table[j].GetKey() == -1)) { //empty or deleted
+				table[j] = hte;
+				return;
+			}
+			else if (table[j].GetKey() == key) { //key exists in table
+				throw new KeyAlreadyExistsException(hte);
+			}
+		} 
+		throw new TableIsFullException(hte);
 	}
 	
 	@Override
 	public void Delete(long key) throws KeyDoesntExistException {
-		// TODO implement deletion
+		for (int i=0; i<table.length; i++) { //search series:
+			int j = Hash(key, i); //the hash result index
+			if (table[j] == null) { //empty cell
+				throw new KeyDoesntExistException(key);
+			}
+			else if (table[j].GetKey() == key) { 
+				table[j] = deleted;
+			}
+		}
+		throw new KeyDoesntExistException(key); //series ended
 	}
 	
 	/**
